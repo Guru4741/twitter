cce7ce06c4f0a7230fe85bed325771aabfd3f64a
Apple <apple@Apples-MacBook-Air.local>
2021-01-28 01:10:22 +0530

    refactor: Refactored HTML and CSS
    Refactors HTML, CSS and JS. Also removes
    unused JS code

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-const bgProfile = document.getElementById('bg-profile')
-let mainElement = document.getElementsByClassName('twitter__profile-section')[0];
-
-
-// fetch('http://localhost:1337/movies/')
-//     .then((response) => {
-//         return response.json()
-//     })
-//     .then(data => {              
-//         bgProfile.style.backgroundImage = `url('http://localhost:1337${data[1].Wallpaper[0].url}')`;
-//     })
+let mainContainer = document.getElementsByClassName('twitter__profile-section')[0];
 
     fetch('http://localhost:1337/posts/')
     .then((response) => {
@@ -17,55 +7,52 @@ let mainElement = document.getElementsByClassName('twitter__profile-section')[0]
     .then(data => {              
 
         data.forEach(each => {
-            const details = {
+            const tweetDetails = {
                 tweet: each.Tweet,
                 date: each.Date,
                 userName: each.UserName,
                 handle: each.Handle,
                 image: `http://localhost:1337${each.Image[0].url}`,
                 user: `http://localhost:1337${each.User[0].url}`
-            }
-
-            console.log(details)
+            }            
             
-            const details2 = `
-            <section class="twitter-tweets">
-            <div class="twitter-tweets__sidebar">
-                <p class="twitter-tweets__retweet">
-                    <i class="fas fa-retweet"></i>
-                </p>
-                <img src="${details.user}" alt="Profile Picture" class="twitter-tweets__img twitter__img">
-            </div>
-            <div class="twitter-tweets__main">
-                <p class="twitter-tweets__profileId">
-                    Monica Bellucci Retweeted
-                </p>
-                <div class="twitter-tweets__tweet-info">
-                    <p class="twitter-tweets__name">
-                        ${details.userName}
+            const tweetHTML = `
+                <section class="twitter-tweets">
+                <div class="twitter-tweets__sidebar">
+                    <p class="twitter-tweets__retweet">
+                        <i class="fas fa-retweet"></i>
                     </p>
-                    <p class="twitter-tweets__handle">
-                        ${details.handle}
+                    <img src="${tweetDetails.user}" alt="Profile Picture" class="twitter-tweets__img twitter__img">
+                </div>
+                <div class="twitter-tweets__main">
+                    <p class="twitter-tweets__profileId">
+                        Monica Bellucci Retweeted
                     </p>
-                    <p class="twitter-tweets__date">
-                        ${details.date}
-                    </p>                        
+                    <div class="twitter-tweets__tweet-info">
+                        <p class="twitter-tweets__name">
+                            ${tweetDetails.userName}
+                        <span class="twitter-tweets__handle">
+                            ${tweetDetails.handle}
+                        </span>
+                        </p>
+                        <p class="twitter-tweets__date">
+                            ${tweetDetails.date}
+                        </p>                        
+                    </div>
+                    <p class="twitter-tweets__tweet-text">
+                        ${tweetDetails.tweet}
+                    </p>
+                    <img src="${tweetDetails.image}" alt="Monica Bellucci" class="twitter-tweets__image">
+                    <div class="twitter-tweets__interaction">
+                        <i class="fas fa-share"></i>
+                        <i class="fas fa-retweet"></i>
+                        <i class="fas fa-heart"></i>
+                    </div>
                 </div>
-                <p class="twitter-tweets__tweet-text">
-                    ${details.tweet}
-                </p>
-                <img src="${details.image}" alt="Monica Bellucci" class="twitter-tweets__image">
-                <div class="twitter-tweets__interaction">
-                    <i class="fas fa-share"></i>
-                    <i class="fas fa-retweet"></i>
-                    <i class="fas fa-heart"></i>
-                </div>
-            </div>
-        </section>
+            </section>
                 `;
     
-            mainElement.innerHTML += details2;
+            mainContainer.innerHTML += tweetHTML;
         })
-
 
     })    
